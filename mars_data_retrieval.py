e2572911f6730fc463e7d34467dafc074c42b478
Daniel Gallagher <danielgallagher500@gmail.com>
2020-10-22 22:08:12 +0100

    wind direction integrated

--- mars_data_retrieval.py
+++ mars_data_retrieval.py
@@ -99,16 +99,78 @@ save_response(resp, save_data_file_path)
 
 #df = pd.read_json(save_data_file_path)
 
+mars_dfs = {}
+
 sols = [str(n) for n in data["sol_keys"]]
-atm_temp = data[sols[0]]["AT"]
 
-cols = ["mn","mx","ct","av"]
+#Atmospheric Temperature
+atm_temp = [data[sol]["AT"] for sol in sols]
+atm_temp_df = pd.DataFrame(atm_temp)
+mars_dfs["AT"] = atm_temp_df
+
+#Horizontal Wind Speed
+hws = [data[sol]["HWS"] for sol in sols]
+hws_df = pd.DataFrame(hws)
+mars_dfs["HWS"] = hws_df
 
 
-atm_temp_df = pd.DataFrame(atm_temp, index=[0])
+#Atmospheric Pressure
+atm_pre = [data[sol]["PRE"] for sol in sols]
+pre_df = pd.DataFrame(atm_pre)
+mars_dfs["PRE"] = pre_df
+
+#Wind Direction
+
+wd_pt_cols = ["compass_degrees", "compass_point", "compass_right", "compass_up", "ct"]
+
+wd_options = [str(x) for x in data[sol]["WD"]]
 
 
-print atm_temp_df
+#WD: Most Common
+wd_most_common = [[data[s]["WD"]["most_common"][k] for k in wd_pt_cols] for s in sols]
+df_wd_most_common = pd.DataFrame(wd_most_common)
+
+mars_dfs["WD"] = {}
+mars_dfs["WD"]["most_common"] = df_wd_most_common
+
+#WD: 
+rose_axes_dict = {}
+axis_dict = {}
+
+for sol in sols:
+	sol_list = []
+	for axis in wd_options:
+		try:
+			axis_data = data[sol]["WD"][axis]
+			axis_data["ax"] = axis
+			sol_list.append(axis_data)
+		except KeyError:
+			print ("Null")
+
+	rose_axes_dict[sol] = pd.DataFrame(sol_list)
+mars_dfs["WD"] = rose_axes_dict
+
+print mars_dfs["WD"]
+
+
+
+"""
+wd_compass_pt_no = [[data[sol]["WD"][str(wind_rose_num)] for wind_rose_num in range(1,16)] for sol in sols]
+
+print wd_compass_pt_no
+
+wd_most_common = [data[sol]["WD"]["most_common"] for sol in sols]
+
+wd_dict = {"compass_pt_no" : pd.DataFrame(wd_compass_pt_no),
+		"most_common" : d.DataFrame(wd_most_common) }
+
+
+print wd_df
+
+
+mars_dfs["WD"] = wd_df
+"""
+
 
 
 
