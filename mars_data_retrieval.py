3c6fe49cc0ef53ace8a47621907b0bb0ad82b5b2
Daniel Gallagher <danielgallagher500@gmail.com>
2020-10-22 15:11:04 +0100

    pd dataframe

--- mars_data_retrieval.py
+++ mars_data_retrieval.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import requests 
+import os 
+import json
 
 
 """
@@ -61,6 +63,16 @@ Time Of Last Datum: Last_UTC (UTC; YYYY-MM-DDTHH:MM:SSZ)
 def successful_response(response):
 	return (response.status_code is 200)
 
+def save_response(response, to_file):
+	with open(to_file, "w") as f:
+		for chunk in resp.iter_content(chunk_size=128):
+			f.write(chunk)
+
+
+
+save_data_file_name = "mars_data.json"
+save_data_file_path = "%s/%s" % (os.getcwd(), save_data_file_name)
+
 #NASA URL & Parameters 
 url = 'https://api.nasa.gov/insight_weather/'
 api_key = "fkkjfgveRAJ2BOVq7gaUAbBbM8omgKo0IRaDEGTj"
@@ -68,7 +80,7 @@ feedtype = "json"
 ver = "1.0"
 
 #Set Parameters
-params = dict(api_key=key,
+params = dict(api_key=api_key,
 			feedtype=feedtype,
 			ver=ver)
 
@@ -80,6 +92,23 @@ data = resp.json()
 
 #Was Data Retrieval Successful?
 successful_resp = successful_response(resp)
+
+save_response(resp, save_data_file_path)
+
+#Create A Dataframe
+
+#df = pd.read_json(save_data_file_path)
+
+sols = [str(n) for n in data["sol_keys"]]
+atm_temp = data[sols[0]]["AT"]
+
+cols = ["mn","mx","ct","av"]
+
+
+atm_temp_df = pd.DataFrame(atm_temp, index=[0])
+
+
+print atm_temp_df
 
 
 
